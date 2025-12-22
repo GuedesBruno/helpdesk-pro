d0df6ebb896f7a471ea5da94ba4413ac9fbda300
GuedesBruno <brunoguedes@tecassistiva.com.br>
2025-12-22 17:16:03 -0300

    Adiciona link explícito do favicon no HTML

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
